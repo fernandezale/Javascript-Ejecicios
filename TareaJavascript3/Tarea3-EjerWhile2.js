4194ba142ac0b9cec8d2013cf18d3c7171090f1b
Alexis <69488773+fernandezale@users.noreply.github.com>
2026-07-01 11:37:22 -0300

    Tarea 3 ejer While 2

--- TareaJavascript3/Tarea3-EjerWhile2.js
+++ TareaJavascript3/Tarea3-EjerWhile2.js
@@ -1,3 +1,7 @@
+/*
+Cuenta regresiva
+Utiliza un ciclo while para mostrar una cuenta regresiva desde 10 hasta 1.
+*/
 let contador = 10;
 while (contador > 0) {
     console.log(`Cuenta🤓: ${contador}`);
