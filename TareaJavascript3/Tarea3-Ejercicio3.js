db3998b6e6b840df382f60e2c8a8005741532f24
Alexis <69488773+fernandezale@users.noreply.github.com>
2026-07-01 11:08:19 -0300

    Tarea 3 ejecicio 3

--- TareaJavascript3/Tarea3-Ejercicio3.js
+++ TareaJavascript3/Tarea3-Ejercicio3.js
@@ -1,3 +1,7 @@
+/*
+Suma de los primeros 10 números 
+Utiliza un ciclo for para sumar los números del 1 al 10 y mostrar el resultado final.
+*/
 let resultado = 0;
 for (let index = 0; index < 10; index++) {
     resultado = resultado + index + 1;
