a160f3dfe4bc85937537c818862fad6c9c06bb89
Alexis <69488773+fernandezale@users.noreply.github.com>
2026-07-18 11:30:18 -0300

    varias correcciones tarea 4 ejer 5

--- TareaJavascript4/Tarea4-Ejercicio5.js
+++ TareaJavascript4/Tarea4-Ejercicio5.js
@@ -8,15 +8,22 @@ Desafío lógico: Debes llevar un control del total acumulado en cada iteración
 */
 let total = 0;
 let eleccion; 
-let continuar = true;1
-while (continuar == true) {
-    precioIngresado = Number(prompt("Ingrese el precio del producto: "));
-    total = total + precioIngresado;
+let continuar = true;
+while (continuar === true) {
+    let precioIngresado = Number(prompt("Ingrese el precio del producto: "));
+    if(precioIngresado > 0)
+    {
+        total = total + precioIngresado;
+        console.log(`Total actual: ${total}`);
+    }
+    else
+    {
+        alert("Ingrese un precio valido.");
+    }
 
-    console.log(`Total actual: ${total}`);
     
     eleccion = prompt("Desea agregar otro producto? si/no :");
-    if(eleccion == "no")
+    if(eleccion === "no")
     {
         continuar = false;
     }
@@ -26,4 +33,8 @@ while (continuar == true) {
 if(total > 5000)
     {
         console.log(`¡Has obtenido un 10% de descuento! El total es: ${total - total/10}`)
+    }
+else
+    {
+        console.log(`Total final sin descuento es: ${total}`);
     }
--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-let intentos = 0;
-while (intentos < 3) {
-    let ingreso = Number(prompt("Ingrese el Token de Seguridad⚠️: "));
-    if(ingreso != 5555)
+let total = 0;
+let eleccion; 
+let continuar = true;
+while (continuar === true) {
+    let precioIngresado = Number(prompt("Ingrese el precio del producto: "));
+    if(precioIngresado > 0)
     {
-        intentos++;
-        console.log(`Token invalido le quedan ${3-intentos} intentos`);
+        total = total + precioIngresado;
+        console.log(`Total actual: ${total}`);
     }
     else
-        {
-            console.log("Token ingresado correctamente👌")
-            break;
-        }
-
-    if(intentos === 3)
     {
-        console.log("❌No hay mas intentos. Acceso denegado!");
+        alert("Ingrese un precio valido.");
     }
+
+    
+    eleccion = prompt("Desea agregar otro producto? si/no :");
+    if(eleccion === "no")
+    {
+        continuar = false;
+    }
+    
 }
+
+if(total > 5000)
+    {
+        console.log(`¡Has obtenido un 10% de descuento! El total es: ${total - total/10}`)
+    }
+else
+    {
+        console.log(`Total final sin descuento es: ${total}`);
+    }
