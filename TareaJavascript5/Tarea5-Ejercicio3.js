d46ca16e6f9d05346c0c5f1d4de21e9f9400aa29
Alexis <69488773+fernandezale@users.noreply.github.com>
2026-07-20 11:50:44 -0300

    Agrego tarea 5 ejercicio 3

--- TareaJavascript5/Tarea5-Ejercicio3.js
+++ TareaJavascript5/Tarea5-Ejercicio3.js
@@ -1,3 +1,9 @@
+/**
+ * Ejercicio 3: El Buscador de Códigos (find)
+Tienes un array de strings que representan códigos de productos. Crea una función llamada buscarProducto que reciba el array y un código específico. La función debe usar .find() para retornar el código si existe, o "No encontrado" si no está.
+
+Array inicial: ["A10", "B20", "C30", "D40"]
+ */
 const listaProducto = ["A10", "B20", "C30", "D40"];
 const buscarProducto = (listaProducto,CodigoBuscado) =>
 {
